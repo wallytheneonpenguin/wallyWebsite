fccc69d9d64b636103aba595394e3658a09d1955
Dean S <deanclownasaur@gmail.com>
2025-06-19 13:42:21 -0600

    Supabase Key

--- src/javascript/retrieve_latest_news.js
+++ src/javascript/retrieve_latest_news.js
@@ -1,10 +1,12 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const news = document.getElementById('latestNews'); 
 
 loadNews();
 
 async function loadNews() { 
-const res = await fetch('/api/latest_news'); 
-  const messages = await res.json(); 
-  news.innerHTML = messages;
+  const supabase = createClient('https://wqoizupthpyffpoflwjn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxb2l6dXB0aHB5ZmZwb2Zsd2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNDMwNTUsImV4cCI6MjA2NTkxOTA1NX0.FdDPG9OtIXgb70ZgT2Vc1enkDgUnryxpb53QRfUg-iE');
+
+  const { data, error } = await supabase.from('latest_news').select();
+  console.log(data);
 }
